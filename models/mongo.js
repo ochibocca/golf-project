2946d456bdb4bfbfd2c804f446a5974d4c509ed1
ochibocca <regan.oo-is.nairobi@stillirisengo.org>
2025-05-30 13:41:35 +0300

    connect mongodb

--- models/mongo.js
+++ models/mongo.js
@@ -2,13 +2,9 @@
 const mongoose = require("mongoose");
 
 module.exports = function connectToMongo() {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
 
     try {
-        mongoose.connect(process.env.MONGO_URL, connectionParams);
+     mongoose.connect(process.env.MONGO_URL);
         console.log("connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
